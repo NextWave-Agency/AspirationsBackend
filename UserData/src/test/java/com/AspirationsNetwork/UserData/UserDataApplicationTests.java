f05e523dba14c822f12d146a890f74831a1c55e3
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2025-12-30 11:56:05 -0600

    Initial Spring Application

---
+++ UserData/src/test/java/com/AspirationsNetwork/UserData/UserDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AspirationsNetwork.UserData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
